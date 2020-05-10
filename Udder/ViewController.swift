5119751417c03f7480dc928d16fac258e4256bd7
umit helva <umithelva@gmail.com>
2020-05-10 14:53:50 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by UMIT HELVA on 10.05.2020.
+//  Copyright © 2020 UMIT HELVA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
